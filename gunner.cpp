7f269bbfddb8383d71b7b196064f2a6c5af92740
zuizuiz <101290372+zuizuiz@users.noreply.github.com>
2022-05-20 01:16:30 +0200

    Update gunner.cpp

--- gunner.cpp
+++ gunner.cpp
@@ -3,16 +3,19 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-SOCKET clientSocket;
+SOCKET client;
 sockaddr_in sockAddress;
+bool toggle = true;
 
 LRESULT CALLBACK hookFunction(int code, WPARAM wParam, LPARAM lParam) {
 
 	KBDLLHOOKSTRUCT* key = (KBDLLHOOKSTRUCT*)lParam;
-
 	char vkchar = (char)MapVirtualKey(key->vkCode, MAPVK_VK_TO_CHAR);
-	
-	if (vkchar == 'W' || vkchar == 'A' || vkchar == 'S' || vkchar == 'D' || vkchar == 'E') {
+
+	if (key->vkCode == VK_F1 && wParam == WM_KEYUP) {
+		toggle = !toggle;
+	}
+	if (toggle && (vkchar == 'W' || vkchar == 'A' || vkchar == 'S' || vkchar == 'D' || vkchar == 'E' || vkchar == 'F' || vkchar == '1' || vkchar == '2' || vkchar == '3' || vkchar == '4')) {
 		char buffer[2];
 
 		buffer[0] = vkchar;
@@ -20,39 +23,37 @@ LRESULT CALLBACK hookFunction(int code, WPARAM wParam, LPARAM lParam) {
 		if (wParam == WM_KEYUP) {
 			buffer[1] = 'B';
 		}
-		send(clientSocket, buffer, sizeof(buffer), 0);
+		send(client, buffer, sizeof(buffer), 0);
 		return -1;
 	}
-	else {
-		return CallNextHookEx(0, code, wParam, lParam);
-	}
+	return CallNextHookEx(0, code, wParam, lParam);
 }
 
 int main() {
 
 	HINSTANCE instance = GetModuleHandle(0);
 	HHOOK hock = SetWindowsHookEx(WH_KEYBOARD_LL, hookFunction, instance, 0);
-	
+
 	WSADATA wsaData;
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) == 0) {
 		std::cout << "WSAStartup\n";
 	}
 
-	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (clientSocket) {
-		std::cout << "clientSocket\n";
+	client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (client) {
+		std::cout << "client\n";
 	}
 
 	sockAddress.sin_family = AF_INET;
 	sockAddress.sin_addr.s_addr = inet_addr("192.168.178.20");
 	sockAddress.sin_port = htons(6969);
-	if (connect(clientSocket, (SOCKADDR*)&sockAddress, sizeof(sockAddress)) == 0) {
-		std::cout << "connected to server\n";
+	if (connect(client, (SOCKADDR*)&sockAddress, sizeof(sockAddress)) == 0) {
+		std::cout << "server\n";
 	}
 
 	MSG msg;
 	while (1) {
-		PeekMessage(&msg, 0, 0, 0, PM_REMOVE);
+		PeekMessage(&msg, 0, 0, 0, 0);
 		Sleep(1);
 	}
 	return 0;
